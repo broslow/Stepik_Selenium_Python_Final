947c8b07cdd4511371983516ce0b615586d11798
John Doe <johndoe@example.com>
2020-02-02 02:58:10 +0500

    Перенесены методы go_to_login_page и should_be_login_link из класса MainPage в BasePage; добавлены тесты перехода на страницу логина (модуль 4, урок 3, шаг 11)

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,5 @@
 import math
+from .locators import BasePageLocators
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import TimeoutException
@@ -34,6 +35,13 @@ class BasePage():
         except TimeoutException:
             return False
         return True
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
--- test_product_page.py
+++ test_product_page.py
@@ -9,36 +9,48 @@ pytest_params = [pytest.param(i, marks=pytest.mark.xfail(i == 7, reason='')) for
 
 ##@pytest.mark.parametrize('link', urls)
 ##def test_guest_can_add_product_to_basket(browser, link):
-@pytest.mark.parametrize('promo_offer', pytest_params)
-def test_guest_can_add_product_to_basket(browser, promo_offer):
-    ##link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_product_to_basket()
-    page.solve_quiz_and_get_code()
-    page.message_after_adding_product_to_basket_should_contain_product_name(page.get_product_name())
-    page.message_after_adding_product_to_basket_should_contain_product_price(page.get_product_price())
+# @pytest.mark.parametrize('promo_offer', pytest_params)
+# def test_guest_can_add_product_to_basket(browser, promo_offer):
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
+    # link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
+    # page = ProductPage(browser, link)
+    # page.open()
+    # page.add_product_to_basket()
+    # page.solve_quiz_and_get_code()
+    # page.message_after_adding_product_to_basket_should_contain_product_name(page.get_product_name())
+    # page.message_after_adding_product_to_basket_should_contain_product_price(page.get_product_price())
 
-@pytest.mark.skip # пропускаем падающий тест
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_product_to_basket()
-    page.message_about_adding_product_to_basket_is_not_presented()
+# @pytest.mark.skip # пропускаем падающий тест
+# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
+    # page = ProductPage(browser, link)
+    # page.open()
+    # page.add_product_to_basket()
+    # page.message_about_adding_product_to_basket_is_not_presented()
 
-def test_guest_cant_see_success_message(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
-    page = ProductPage(browser, link)
-    page.open()
-    page.message_about_adding_product_to_basket_is_not_presented()
+# def test_guest_cant_see_success_message(browser):
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
+    # page = ProductPage(browser, link)
+    # page.open()
+    # page.message_about_adding_product_to_basket_is_not_presented()
 
-@pytest.mark.skip # пропускаем падающий тест
-def test_message_disappeared_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
+# @pytest.mark.skip # пропускаем падающий тест
+# def test_message_disappeared_after_adding_product_to_basket(browser):
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
+    # page = ProductPage(browser, link)
+    # page.open()
+    # page.add_product_to_basket()
+    # time.sleep(1)
+    # page.message_about_adding_product_to_basket_is_disappeared()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
-    page.add_product_to_basket()
-    time.sleep(1)
-    page.message_about_adding_product_to_basket_is_disappeared()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
