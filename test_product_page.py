3f299b2cb49c88fe7dd09091be83ee906b1a63d5
John Doe <johndoe@example.com>
2020-02-01 21:37:20 +0500

    Параметризация теста для сообщений после добавления продукта в корзину (модуль 4, урок 3, шаг 4)

--- test_product_page.py
+++ test_product_page.py
@@ -1,12 +1,22 @@
 from .pages.product_page import ProductPage
-#import time
+import pytest
+##import time
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
+#product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+#urls = [f"{product_base_link}/?promo=offer{no}" for no in range(10)]
+pytest_params = [pytest.param(i, marks=pytest.mark.xfail(i == 7, reason='')) for i in range(10)]
+
+
+#@pytest.mark.parametrize('link', urls)
+#def test_guest_can_add_product_to_basket(browser, link):
+@pytest.mark.parametrize('promo_offer', pytest_params)
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
     page.message_after_adding_product_to_basket_should_contain_product_name(page.get_product_name())
     page.message_after_adding_product_to_basket_should_contain_product_price(page.get_product_price())
-    #time.sleep(2500)
+    ##time.sleep(2500)
